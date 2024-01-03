4220f859484fe29d1fece85ca15b10641626e13f
KAgaev <kenkosh@yandex.ru>
2024-01-04 00:10:18 +0300

    ws exception filters

---
+++ api/src/common/filters/ws-exceptions/ws-exceptions.filter.spec.ts
@@ -0,0 +1,7 @@
+import { WsExceptionsFilter } from './ws-exceptions.filter';
+
+describe('WsExceptionsFilter', () => {
+  it('should be defined', () => {
+    expect(new WsExceptionsFilter()).toBeDefined();
+  });
+});
---
+++ api/src/common/filters/ws-exceptions/ws-exceptions.filter.ts
@@ -0,0 +1,25 @@
+import { ArgumentsHost, Catch } from '@nestjs/common';
+import { BaseWsExceptionFilter } from '@nestjs/websockets';
+import { LoggerService } from '@common/logger';
+
+export interface IWsException {
+  response: {
+    message: string[];
+    error: string;
+    statusCode: number;
+  };
+  status: number;
+}
+
+// TODO дополнить сообщение об ошибке
+@Catch()
+export class WsExceptionsFilter extends BaseWsExceptionFilter<IWsException> {
+  private readonly logger = new LoggerService();
+
+  catch(exception: IWsException, host: ArgumentsHost) {
+    const message = exception.response.message.join('; ');
+    this.logger.error(message, WsExceptionsFilter.name);
+
+    super.catch(exception, host);
+  }
+}
